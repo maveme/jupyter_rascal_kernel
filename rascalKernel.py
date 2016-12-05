6fd9b06321eede74740665ae3a6de34d13502c7f
Mauricio Verano Merino <mauricioveranom@me.com>
2016-12-05 16:55:22 +0100

    First commit with the Rascal Kernel using a Python wrapper kernel

---
+++ rascalKernel.py
@@ -0,0 +1,84 @@
+from ipykernel.kernelbase import Kernel
+import os
+from pexpect import replwrap, EOF
+import signal
+
+
+class RascalKernel(Kernel):
+    implementation = ''
+    implementation_version = ''
+    language = 'Rascal'
+    language_version = ''
+    banner = language + language_version + '\n'
+    language_info = {
+        'name': 'rascal',
+        'codemirror_mode': 'java',
+        'mimetype': 'p',
+        'file_extension': '.rsc'
+    }
+
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self._start_rascal()
+
+    # _JAVA_COMMAND = '{}/bin/java'.format(os.environ['JAVA_HOME'])
+    _JAVA_COMMAND = "/Library/Java/JavaVirtualMachines/jdk1.8.0_77.jdk/Contents/Home/bin/java"
+    # _RASCAL_LOCATION = os.environ['RASCAL_LOCATION']
+    _RASCAL_LOCATION = "/Users/Mauricio/Downloads/rascal-shell-stable.jar"
+
+    def _start_rascal(self):
+        sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
+        try:
+            self.rascal_wrapper = replwrap.REPLWrapper("{} -jar {}".format(self._JAVA_COMMAND,
+                                                                           self._RASCAL_LOCATION
+                                                                           ), "rascal>", None)
+        finally:
+            signal.signal(signal.SIGINT, sig)
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        if not code.strip():
+            return {
+                'status': 'OK',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {}
+            }
+        interrupted = False
+        try:
+            output = self.rascal_wrapper.run_command(code.rstrip(), timeout=None)
+        except KeyboardInterrupt:
+            self.rascal_wrapper.child.sendintr()
+            interrupted = True
+            self.rascal_wrapper._expect_prompt()
+            output = self.rascal_wrapper.child.before
+        except EOF:
+            output = self.rascal_wrapper.child.before + 'Restarting Rascal'
+            self._start_rascal()
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+        if interrupted:
+            return 'Abort'
+        try:
+            exitcode = int(self.rascal_wrapper.run_command('1').rstrip())
+        except Exception:
+            exitcode = 1
+
+        if exitcode:
+            error_content = {'execution_count': self.execution_count,
+                             'ename': '', 'evalue': str(exitcode),
+                             'traceback': []}
+
+            self.send_response(self.iopub_socket, 'error', error_content)
+            error_content['status'] = 'Exception'
+            return error_content
+        else:
+            return {'status': 'ok', 'execution_count': self.execution_count,
+                    'payload': [], 'user_expressions': {}}
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+
+    IPKernelApp.launch_instance(kernel_class=RascalKernel)
